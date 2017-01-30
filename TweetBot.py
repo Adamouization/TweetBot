0db12a3a9d25037d8246141f2508f62b9c167bd0
Adamouization <aj645@bath.ac.uk>
2017-01-30 11:12:58 +0100

    removed keys from git rep + added them to gitignore file

--- TweetBot.py
+++ TweetBot.py
@@ -1,15 +1,11 @@
 #!/usr/bin/env python
+import keys
 import sys
 import tweepy
 import os
 
-CONSUMER_KEY = 'Skn7K6EscqGwlqNE7YFvugFHv'
-CONSUMER_SECRET = 'FO2RvQG9sTgyuMx5fYs1XLL65B3yiYBTTxf1f4YcbpSlAfg1MY'
-ACCESS_KEY = '825827462038880261-jg5OQZxmMjFPcNDWhwNL0XJ6lx24DtD'
-ACCESS_SECRET = 'rxR7RveK8ECbP6UVkzQTltUkqlM5L3QxluG6gUiETUf1p'
-
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+auth = tweepy.OAuthHandler(keys.CONSUMER_KEY, keys.CONSUMER_SECRET)
+auth.set_access_token(keys.ACCESS_KEY, keys.ACCESS_SECRET)
 
 api = tweepy.API(auth)
 cmd = '/opt/vc/bin/vcgencmd measure_temp'
