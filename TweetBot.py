7489a2da50ac816cdb34354bb2bda9dfac20084a
Adamouization <aj645@bath.ac.uk>
2017-01-30 14:15:33 +0100

    tweet sentences randomly from a text file + switched to twython + added comments

--- TweetBot.py
+++ TweetBot.py
@@ -1,15 +1,40 @@
 #!/usr/bin/env python
+# imports
 import keys
 import sys
-import tweepy
+#import tweepy
 import os
+import random
+from twython import Twython
 
-auth = tweepy.OAuthHandler(keys.CONSUMER_KEY, keys.CONSUMER_SECRET)
-auth.set_access_token(keys.ACCESS_KEY, keys.ACCESS_SECRET)
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
-api = tweepy.API(auth)
-cmd = '/opt/vc/bin/vcgencmd measure_temp'
-line = os.popen(cmd).readline().strip()
-temp = line.split('=')[1].split("'")[0]
-api.update_status(status='My current Raspberry Pi CPU temperature is ' + temp + ' C')
+# authentication to twitter application using local keys
+#auth = tweepy.OAuthHandler(keys.CONSUMER_KEY, keys.CONSUMER_SECRET)
+#auth.set_access_token(keys.ACCESS_KEY, keys.ACCESS_SECRET)
+#api = tweepy.API(auth)
+api = Twython(keys.CONSUMER_KEY,keys.CONSUMER_SECRET,keys.ACCESS_KEY,keys.ACCESS_SECRET)
+
+# retrieve CPU temperature
+#cmd = '/opt/vc/bin/vcgencmd measure_temp'
+#line = os.popen(cmd).readline().strip()
+#temp = line.split('=')[1].split("'")[0]
+
+# tweet random sentences retrieved from a txt file
+def randomTweet():
+	try:
+		tweetsFile = open(os.path.join(__location__,'tweets.txt'),'r')
+		tweetsList = tweetsFile.readlines()
+		tweetsFile.close()
+		randomChoice = random.randrange(len(tweetsList))
+		print(tweetsList[randomChoice]) #debugging
+		api.update_status(status=tweetsList[randomChoice])
+		return None
+	except IOError:	
+		return None
+
+randomTweet()
+
+# update twitter status
+#api.update_status(status='My current Raspberry Pi CPU temperature is ' + temp + ' C')
 #api.update_status(sys.argv[1])
