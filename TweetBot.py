073887bd466dd7a83ea7e14ec70375df077d21f7
Adamouization <aj645@bath.ac.uk>
2017-01-30 23:46:24 +0100

    added comments

--- TweetBot.py
+++ TweetBot.py
@@ -2,7 +2,6 @@
 # imports
 import keys
 import sys
-#import tweepy
 import os
 import random
 from twython import Twython
@@ -10,15 +9,12 @@ from twython import Twython
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
 # authentication to twitter application using local keys
-#auth = tweepy.OAuthHandler(keys.CONSUMER_KEY, keys.CONSUMER_SECRET)
-#auth.set_access_token(keys.ACCESS_KEY, keys.ACCESS_SECRET)
-#api = tweepy.API(auth)
 api = Twython(keys.CONSUMER_KEY,keys.CONSUMER_SECRET,keys.ACCESS_KEY,keys.ACCESS_SECRET)
 
 # retrieve CPU temperature
-#cmd = '/opt/vc/bin/vcgencmd measure_temp'
-#line = os.popen(cmd).readline().strip()
-#temp = line.split('=')[1].split("'")[0]
+cmd = '/opt/vc/bin/vcgencmd measure_temp'
+line = os.popen(cmd).readline().strip()
+temp = line.split('=')[1].split("'")[0]
 
 # tweet random sentences retrieved from a txt file
 def randomTweet():
@@ -28,13 +24,12 @@ def randomTweet():
 		tweetsFile.close()
 		randomChoice = random.randrange(len(tweetsList))
 		print(tweetsList[randomChoice]) #debugging
-		api.update_status(status=tweetsList[randomChoice])
+		api.update_status(status=tweetsList[randomChoice]) #update twitter status
 		return None
 	except IOError:	
 		return None
 
 randomTweet()
 
-# update twitter status
 #api.update_status(status='My current Raspberry Pi CPU temperature is ' + temp + ' C')
-#api.update_status(sys.argv[1])
+#api.update_status(sys.argv[1]) #tweet line input
