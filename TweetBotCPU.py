b94cf5f38a505db592dbc19698be28e29f490cc5
Adamouization <aj645@bath.ac.uk>
2017-02-07 12:47:33 +0100

    fixed CPU tweetbot

--- TweetBotCPU.py
+++ TweetBotCPU.py
@@ -12,7 +12,12 @@ degree = unichr(176) # degree symbol
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
 # authentication to twitter application using local keys
-api = Twython(keys.CONSUMER_KEY,keys.CONSUMER_SECRET,keys.ACCESS_KEY,keys.ACCESS_SECRET)
+api = Twython(
+	keys.CONSUMER_KEY,
+	keys.CONSUMER_SECRET,
+	keys.ACCESS_KEY,
+	keys.ACCESS_SECRET
+)
 
 # time
 now = i.strftime('%Y/%m/%d %H:%M:%S')
@@ -21,4 +26,6 @@ cmd = '/opt/vc/bin/vcgencmd measure_temp'
 line = os.popen(cmd).readline().strip()
 temp = line.split('=')[1].split("'")[0]
 
-api.update_status(status= now + 'My current CPU temperature is ' + temp + ' ' + degree + 'C') #tweet cpu temp
+tweet = now + ' My current CPU temperature is ' + temp + ' ' + degree + 'C'
+print(tweet) # debugging
+api.update_status(status = tweet) # tweet CPU temp
