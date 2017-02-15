44bd13916c0358a1e7ed147bc05b5d1a892ef3b5
Adamouization <aj645@bath.ac.uk>
2017-02-15 23:54:43 +0000

    optimise cpu temp tweeting script + remove pictues from online rep

--- TweetBotCPU.py
+++ TweetBotCPU.py
@@ -6,13 +6,18 @@ import os
 import random
 import tweepy
 from datetime import datetime
-from subprocess import call
 
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
 # authentication to twitter application using local keys
-auth = tweepy.OAuthHandler(keys.CONSUMER_KEY, keys.CONSUMER_SECRET)
-auth.set_access_token(keys.ACCESS_KEY, keys.ACCESS_SECRET)
+auth = tweepy.OAuthHandler(
+	keys.CONSUMER_KEY, 
+	keys.CONSUMER_SECRET
+)
+auth.set_access_token(
+	keys.ACCESS_KEY, 
+	keys.ACCESS_SECRET
+)
 api = tweepy.API(auth)
 
 # time
@@ -26,7 +31,8 @@ line = os.popen(cmd).readline().strip()
 temp = line.split('=')[1].split("'")[0]
 
 # picture
-#picture_path = '/home/pi/Documents/PythonProjects/TweetBot/Pictures/' + photo_name
+pic_name = "temperature" + ".jpg"
+picture_path = '/home/pi/Documents/PythonProjects/TweetBot/pictures/' + pic_name
 
 # generate tweet to send with time, temp and degree symbol
 tweet = now + ' My current CPU temperature is ' + temp + ' ' + degree + 'C'
@@ -41,4 +47,4 @@ print(tweet) # debugging
 #fo.close()
 
 # tweet CPU temp
-api.update_status(tweet)
+api.update_with_media(picture_path, status=tweet)
