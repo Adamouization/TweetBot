d866c165dd944c45a8a62e70e91442edff909db4
Adamouization <aj645@bath.ac.uk>
2017-02-15 23:46:40 +0000

    switched from Twython to Tweepy for CPU tweet script

--- TweetBotCPU.py
+++ TweetBotCPU.py
@@ -4,28 +4,31 @@ import keys
 import sys
 import os
 import random
-from twython import Twython
+import tweepy
 from datetime import datetime
+from subprocess import call
 
-i = datetime.now()
-degree = unichr(176) # degree symbol
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
 # authentication to twitter application using local keys
-api = Twython(
-	keys.CONSUMER_KEY,
-	keys.CONSUMER_SECRET,
-	keys.ACCESS_KEY,
-	keys.ACCESS_SECRET
-)
+auth = tweepy.OAuthHandler(keys.CONSUMER_KEY, keys.CONSUMER_SECRET)
+auth.set_access_token(keys.ACCESS_KEY, keys.ACCESS_SECRET)
+api = tweepy.API(auth)
 
 # time
+i = datetime.now()
 now = i.strftime('%Y/%m/%d %H:%M:%S')
+
 # retrieve CPU temperature
+degree = unichr(176) #degree symbol
 cmd = '/opt/vc/bin/vcgencmd measure_temp'
 line = os.popen(cmd).readline().strip()
 temp = line.split('=')[1].split("'")[0]
 
+# picture
+#picture_path = '/home/pi/Documents/PythonProjects/TweetBot/Pictures/' + photo_name
+
+# generate tweet to send with time, temp and degree symbol
 tweet = now + ' My current CPU temperature is ' + temp + ' ' + degree + 'C'
 print(tweet) # debugging
 
@@ -38,4 +41,4 @@ print(tweet) # debugging
 #fo.close()
 
 # tweet CPU temp
-api.update_status(status = tweet)
+api.update_status(tweet)
